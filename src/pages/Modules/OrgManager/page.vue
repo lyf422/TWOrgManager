cf6c385260b0f6f755ecc0090c2771802464e3d6
sh963852741 <sh963852741@126.com>
2020-01-13 23:10:44 +0800

    删除选择组件

--- src/pages/Modules/OrgManager/page.vue
+++ src/pages/Modules/OrgManager/page.vue
@@ -46,20 +46,12 @@
                 子部门:{{data.children}}
             </i-row>
         </i-col>
-        <i-card>
-        <!--
-            这个组件很得重要，他就是iView里的select控件，只不过多一个选项。选项内容进入后台“系统配置”->“数据字典配置”，里面的“字典归属”。
-            比如在下面的示例中，dic为“政治面貌”那么就相当于在后台那里，“字典归属”为“政治面貌”的值的集合。
-            之后，由其是在“修改社团信息”页面中，所有相关的选项都应当使用这个东西。比如“政治面貌”，“社团类型”，“指导老师类型”或者“社交媒体”等。
-            具体使用哪一个，我会有下发的需求卡中有说明。
-        -->
-        </i-card>
     </i-row>
 </template>
 
 <script>
 import axios from 'axios';
-var app = require("@/config");
+let app = require("@/config");
 export default {
     data () {
         return {
@@ -127,8 +119,8 @@ export default {
         getDashBoard () {
             axios.post("/api/org/GetDashboard", {}, msg => {
                 this.data = msg;
+                app.departType = msg.DepartType;
             });
-            console.log(this.data.DepartType);
         },
         judgeTime () {
             var day2 = new Date();

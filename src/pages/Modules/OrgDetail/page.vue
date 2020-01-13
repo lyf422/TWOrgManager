45c65ce67c711fe142a5acaa51373918c605fdb5
sh963852741 <sh963852741@126.com>
2020-01-13 21:01:32 +0800

    修改bug

--- src/pages/Modules/OrgDetail/page.vue
+++ src/pages/Modules/OrgDetail/page.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>123</div>
+    <i-row>
+        <i-card>
+            <i-row>
+                <i-col span=4>图片</i-col>
+                <i-col span=20>
+                    <i-row>社团名称</i-row>
+                    <i-row>成员人数/指导老师</i-row>
+                </i-col>
+            </i-row>
+        </i-card>
+        <i-divider/>
+        <i-card>
+            <i-tabs value="name1">
+                <i-tab-pane label="基本信息" name="name1">标签一的内容</i-tab-pane>
+                <i-tab-pane label="成员管理" name="name2">标签二的内容</i-tab-pane>
+                <i-tab-pane label="子部门" name="name3">标签三的内容</i-tab-pane>
+                <i-tab-pane label="指导老师" name="name3">标签三的内容</i-tab-pane>
+                <i-tab-pane label="管理员" name="name4">标签四的内容</i-tab-pane>
+                <i-tab-pane label="活动经费" name="name5">标签五的内容</i-tab-pane>
+            </i-tabs>
+        </i-card>
+    </i-row>
 </template>
 
 <script>

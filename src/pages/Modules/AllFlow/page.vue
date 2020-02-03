117c8702d7b9361bf9e807fa9fe18c3adea7ee79
宋润涵 <53166321+sh963852741@users.noreply.github.com>
2020-02-03 17:44:45 +0800

    流程查看可用

---
+++ src/pages/Modules/AllFlow/page.vue
@@ -0,0 +1,52 @@
+<template>
+    <i-table :columns="columns" :data="tableData">
+        <template slot="Action" slot-scope="{row}">
+            <i-button @click="dealWorkflow(row.InstanceId, row.StepId)">执行</i-button>
+        </template>
+    </i-table>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    data () {
+        return {
+            tableData: [],
+            columns: [
+                {
+                    title: "流程类别",
+                    key: "WorkflowName"
+                },
+                {
+                    title: "创建者",
+                    key: "Owner"
+                },
+                {
+                    title: "创建时间",
+                    key: "ArriveOn"
+                },
+                {
+                    title: "当前状态",
+                    key: "State"
+                },
+                {
+                    title: "操作",
+                    slot: "Action"
+                }
+            ]
+        }
+    },
+    mounted () {
+        this.getFlows();
+    },
+    methods: {
+        getFlows () {
+            axios.post("/api/workflow/AllFlow", {}, msg => {
+                this.tableData = msg.data;
+            });
+        },
+        dealWorkflow (instanceId, stepId) {
+            window.open("/manage/org/activityform?instanceId=" + instanceId + '&stepId=' + stepId);
+        }
+    }
+}
+</script>
